d79c271dc35dd67241e5db88dee61a764e66fb76
anushreeSonak <sonakanushree@gmail.com>
2023-04-25 16:25:48 +0530

    Added updated code of Assignemnt no-1

--- APIAssignment/src/test/java/APIAssignments/Product.java
+++ APIAssignment/src/test/java/APIAssignments/Product.java
@@ -1,28 +1,67 @@
 package APIAssignments;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.logging.*;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 
 public class Product {
-    @BeforeTest
-    public void getURL() {
-        baseURI = "https://automationexercise.com/api/productsList";
+    private static String url;
+
+    {
+        try {
+            url = ConfigReader.getUrl();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @Test
-    public void validateProductDetails() {
-        String getList = given().when().get(baseURI).then().log().all().toString();
-        System.out.println(getList);
-        var getStatusCode = given().when().get(baseURI).then().assertThat().log().all().statusCode(200);
-        System.out.println(getStatusCode);
+    private Logger logger = Logger.getLogger("Product.class");
+
+    @Test(priority = 1)
+    public void validateProductList() {
+        baseURI = url;
+        var getALLProductList = given().when().get(baseURI).then().log().all().toString();
+        logger.log(Level.INFO, "List return by API is " + getALLProductList);
     }
 
-    public static void main(String argu[]) {
-        Product product = new Product();
-        product.validateProductDetails();
+    @Test(priority = 2)
+    public void validateStatusCode() {
+        baseURI = url;
+        logger.info("getAllProductsList baseURI:" + baseURI);
+        RequestSpecification httpRequest = given();
+        Response response = httpRequest.request(Method.GET);
+        var getStatusCode = response.getStatusCode();
+        logger.log(Level.INFO, "Status Code is : " + getStatusCode);
+        Assert.assertEquals(getStatusCode, 200);
+    }
+
+    @Test(priority = 3)
+    public void validateContent() {
+        Response response = given()
+                .contentType(ContentType.JSON).when().get(url);
+        Assert.assertEquals(response.getStatusCode(), 200);
+        var productName = response.jsonPath().get("products[5].name").toString();
+        Assert.assertEquals(productName, " Summer White Top");
+        logger.log(Level.INFO, "Product name=" + productName);
+    }
+
+    @Test(priority = 4)
+    public void validateLength() {
+        var response = given().when().get(url).then().extract().asString();
+        JsonPath jsonResponse = new JsonPath(response);
+        var idLength = jsonResponse.getInt("products.id.size()");
+        Assert.assertEquals(idLength, 34, "number of products are not expected");
+        logger.log(Level.INFO, "The length is : " + idLength + " products");
     }
 }
