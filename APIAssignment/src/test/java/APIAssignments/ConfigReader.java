d40a6269c92698ddb3dada0c7b438661a5f3edbe
Anushree Sonak <anushrees@synerzipune.local>
2023-04-27 18:20:48 +0530

    Added ConfigReader file

--- APIAssignment/src/test/java/APIAssignments/ConfigReader.java
+++ APIAssignment/src/test/java/APIAssignments/ConfigReader.java
@@ -1,2 +1,17 @@
-package APIAssignments;public class ConfigReader {
+package APIAssignments;
+public class ConfigReader
+{
+    public static Properties getProperties() throws IOException {
+        FileInputStream file = new FileInputStream("");
+        Properties properties = new Properties();
+        properties.load(file);
+        return properties;
+    }
+
+
+    public static String getUrl() throws IOException {
+        return getProperties().getProperty("getUrl");
+    }
+}
+ 
 }
