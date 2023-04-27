953e5e92f8bbcb1295fcb60f642e5a79bc0922b2
Anushree Sonak <anushrees@synerzipune.local>
2023-04-27 18:50:16 +0530

    Added config reader file

--- APIAssignment/src/test/java/APIAssignments/ConfigReader.java
+++ APIAssignment/src/test/java/APIAssignments/ConfigReader.java
@@ -1,17 +1,18 @@
 package APIAssignments;
-public class ConfigReader
-{
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
     public static Properties getProperties() throws IOException {
-        FileInputStream file = new FileInputStream("");
+        FileInputStream file = new FileInputStream("C:\\Users\\anushrees\\Desktop\\APIAutomation\\APIAutomation\\APIAssignment\\config.properties");
         Properties properties = new Properties();
         properties.load(file);
         return properties;
     }
 
-
     public static String getUrl() throws IOException {
-        return getProperties().getProperty("getUrl");
+        return getProperties().getProperty("baseURL");
     }
-}
- 
 }
