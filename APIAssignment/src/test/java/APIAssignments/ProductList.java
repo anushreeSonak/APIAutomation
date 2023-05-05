f8f88bb29e0b6ab44ee6a876a4c951c8d793e717
Anushree Sonak <anushrees@synerzipune.local>
2023-05-05 16:20:17 +0530

    Added code of Assignmnet no 2

--- APIAssignment/src/test/java/APIAssignments/ProductList.java
+++ APIAssignment/src/test/java/APIAssignments/ProductList.java
@@ -1,10 +1,21 @@
 package APIAssignments;
 
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.apache.log4j.PropertyConfigurator;
+import org.json.JSONObject;
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+//import io.restassured.response.Response;
+//import org.apache.log4j.PropertyConfigurator;
+//import org.testng.Assert;
+//import org.testng.annotations.BeforeTest;
+//import org.testng.annotations.Test;
 
 import java.io.IOException;
+//import java.io.ObjectInputFilter;
 import java.util.logging.Logger;
 
 import static io.restassured.RestAssured.given;
@@ -12,7 +23,7 @@ import static io.restassured.RestAssured.given;
 public class ProductList {
     private static String url;
 
-    {
+    public ProductList() {
         try {
             url = ConfigReader.getUrl();
         } catch (IOException e) {
@@ -20,17 +31,29 @@ public class ProductList {
         }
     }
 
-    private Logger logger = Logger.getLogger("ProductList.class");
+    private static Logger logger = Logger.getLogger("PostProduct.class");
 
-    @Test
-    public void validateStatusCode() {
-        Response response = given().queryParam("Sort", "name")
-                .when().post(url).then().log().body().statusCode(200).extract().response();
-        String value = response.asString();
-        Assert.assertEquals(response.getStatusCode(), 200);
-        logger.info("Status code of " + url + " is " + response.getStatusCode());
-        Assert.assertEquals(value.contains("This request method is not supported."), true, "Values does not contains \"This request method is not supported.\"");
-        Assert.assertEquals(value.contains("\"responseCode\": 405"), true, "Values does not contains \"responseCode\": 405");
-        logger.info("Values contains \"responseCode\": 405");
+    @BeforeTest
+    public void getLoggerDisplay() {
+        PropertyConfigurator.configure("log4j2.properties");
+    }
+
+    @Test(priority = 1)
+    public void validateResponseCode() {
+        Response response = given().when().post(url);
+        var result = response.body().asString();
+        JSONObject jsonObject = new JSONObject(result);
+        var responseCode = jsonObject.get("responseCode");
+        logger.info("responseCode: " + responseCode);
+        Assert.assertEquals(responseCode, 405);
+    }
+
+    @Test(priority = 2)
+    public void validateResponseMessage() {
+        var response = given().when().post(url).then().extract().asString();
+        JsonPath jsonResponse = new JsonPath(response);
+        var responseMessage = jsonResponse.get("message");
+        logger.info("Response Message is" + responseMessage);
+        Assert.assertEquals(responseMessage, "This request method is not supported.");
     }
 }
